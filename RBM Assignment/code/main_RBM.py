950750ecdbdea6758af72ea7536cdd2dc6ea2ead
kamalelsaaid <kamal.zakieldin@arete-global.com>
2019-02-14 22:10:04 +0200

    model algorithm

--- RBM Assignment/code/main_RBM.py
+++ RBM Assignment/code/main_RBM.py
@@ -1,10 +1,28 @@
+'''
+1- load the training set ( the images and the labels )
+2- initialize the weights / hidden h & visible v units
+3- generate random samples by gibbs sampler
+4- 
+	a- +ve phase
+		sample from the dataset
+		update hidden by the probability of hidden given visible p(h|v)
+		calculate the gredient for the +ve phase
+	b- -ve phase
+		sample from the model
+		repeat K times ( update hidden by the probability of hidden given visible p(h|v) )
+		calculate the gredient for the -ve phase
+	c- update the weights
+		w = w + lr(+ve Gredient - -ve Gredient)
+'''
+
 import numpy as np
 
-
-
 # generate the dataset
+def load_data():
+	dt = np.dtype('>u4, >u4, >u4, >u4, (10000,784)u1')
+	mnist = np.fromfile('t10k-images-idx3-ubyte', dtype=dt)['f4'][0].T
+	imgs = np.zeros((784, 10000), dtype=np.dtype('b'))
+	imgs[mnist > 127] = 1
 
-dt = np.dtype('>u4, >u4, >u4, >u4, (10000,784)u1')
-mnist = np.fromfile('t10k-images-idx3-ubyte', dtype=dt)['f4'][0].T
-imgs = np.zeros((784, 10000), dtype=np.dtype('b'))
-imgs[mnist > 127] = 1
+def sigmoid_fun(X):
+    return 1./(1.+np.exp(-X))
